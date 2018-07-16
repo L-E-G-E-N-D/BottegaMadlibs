3d380d5c54a707219335ff2ffd27fab5867b0f15
DevCamp <DevCamp@708S-1023B-12.canyonsdistrict.pvt>
2018-07-16 10:03:38 -0600

    Added Content Visible State and Generate and Clear Funcitonality

--- src/components/card.js
+++ src/components/card.js
@@ -13,28 +13,31 @@ class Card extends Component {
             pluralNoun: '',
             adjectiveOne: '',
             celebOne: '',
-
             adjectiveTwo: '',
             nounOne : '',
             numberOne : '',
             numberTwo : '',
-
             nounTwo: '',
             adjectiveThree: '',
             celebTwo: '',
             celebThree: '',
-
             adjectiveFour: '',
             nounThree: '',
             celebFour: '',
-            adjectiveFive: ''
+            adjectiveFive: '',
+            contentVisible: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
     } 
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value })
+    }
+    handleFormSubmit(event) {
+        event.preventDefault()
+        this.setState({ contentVisible: !this.state.contentVisible })
     }
 
     render() {
@@ -61,12 +64,17 @@ class Card extends Component {
             {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
         ]
         return (
-            <div className="card">
+            <form onSubmit={this.handleFormSubmit} className="card">
+                <div className="card__inputs">
+                    {
+                        inputData.map(data => Input( (data), this.handleInputChange))
+                    }
+                </div>
+                <button type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Mad Lib'}</button>
                 {
-                    inputData.map(data => Input( (data), this.handleInputChange))
+                    this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
-                <Content data={this.state}/>
-            </div>
+            </form>
         )
     }
 }
