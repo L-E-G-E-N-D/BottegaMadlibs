f7152089e14fe25bc4e7d2703d404b71a470730e
DevCamp <DevCamp@708S-1023B-12.canyonsdistrict.pvt>
2018-07-16 11:41:14 -0600

    Added Header and Paragraph Styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ const Header = () => {
         <div className="header">
         <div className="header__skew"></div>
         <div className="header__border"></div>
-            <h1>Bottega Madlibs</h1>
-            <p>Fill ut the feilds below and click the generate button to see the Mad Lib Story.</p>
+            <h1>Bottega Mad Libs</h1>
+            <p>Fill out the feilds below and click the generate button to see the Mad Lib Story.</p>
         </div>
     )
 }
