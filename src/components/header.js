e41202f327c939258b0b559e4f988ec2e78f1c2f
DevCamp <DevCamp@708S-1023B-12.canyonsdistrict.pvt>
2018-07-16 11:30:50 -0600

    Created background header skews

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+        <div className="header__skew"></div>
+        <div className="header__border"></div>
             <h1>Bottega Madlibs</h1>
             <p>Fill ut the feilds below and click the generate button to see the Mad Lib Story.</p>
         </div>
